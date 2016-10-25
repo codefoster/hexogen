3a729744858a1226b2ae10b08275cc33e719d04d
Jeremy Foster <jeremy.foster@live.com>
2016-10-24 21:58:14 -0700

    Site updated: 2016-10-24 21:58:14

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/'));
+console.log('__dirname: ' + __dirname);
+app.use(express.static(__dirname));
 app.listen(process.env.PORT);
